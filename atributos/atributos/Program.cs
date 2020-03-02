8f2fb4e8dff2fa2683f8d9275e5b3056e77f8b8e
edward_guzman <jhonedward456@gmail.com>
2020-03-02 11:51:08 -0500

    Guardar ejercicio atributos

---
+++ atributos/atributos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        aprendices apSena = new aprendices();
+        apSena.nombres = "  JHON EDWARD ";
+        apSena.apellidos = " GUZMAN CHAVARRO ";
+        apSena.direccion = " VEREDAD HIGUERON ";
+        saludo(apSena);
+        }
+        private  static void saludo (aprendices apSena)
+        {
+        Console.WriteLine(" Bienvenido. sr(a)" + apSena.nombres + apSena.apellidos);
+        Console.WriteLine(" su direccion de residencia es: " + apSena.direccion);
+        Console.ReadLine();
+
+        }
+
+
+  }
+
